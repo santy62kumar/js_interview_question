a0561837c92c7396b352980071b76b0505612778
santy62kumar <s.santoshkumar6205@gmail.com>
2024-03-29 12:03:08 +0530

    split reduce,sort,trim

---
+++ 01day.js
@@ -0,0 +1,35 @@
+// we to find the longest words in a sentense, if there are multiple then return first encountered
+// the function should ignore leading and trailing whitespaces 
+//if the input string is empty or whitespaces only then return false
+
+const findLongestWord=(str)=>{
+
+    //The trim() method removes whitespace from both sides of a string.
+    //The trimEnd() Method
+
+    // The trimStart() Method
+
+    if(str.trim().length===0){
+        return false;
+    }
+
+    // str=str.trim();
+    // console.log(str);
+
+    word=str.split(" "); // this will convert string  to array 
+    word=word.sort((a,b)=> b.length-a.length);
+    console.log(word);
+
+    //return word.at(-1); // this is used when we return a.length-b.length
+   return word[0];
+
+   //return word.reduce((accum,curword)=> (curword.length> accum.length ? curword :accum), "");
+   
+   //coins.reduce( (accumulator, currentElement) => { }, initialValue )
+
+   // link to refer for reduce concept:"https://medium.com/@jennyjean8675309/exploring-javascripts-reduce-method-part-1-84a8e804020e"
+
+}
+// Example usage
+var sentence = "This is a sentence with some long words";
+console.log(findLongestWord(sentence)); // Output: "sentence"
